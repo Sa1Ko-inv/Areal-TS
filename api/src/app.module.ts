c775e0824c81cd2879b7f44c1eae0691251c1b65
Dmitriy Blinow <dmitriyblinow@mail.ru>
2025-07-16 13:45:39 +0300

    Transition to ORM prisma

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,18 +2,14 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
-import { DatabaseService } from './config/database.service'
-import { EmployeeModule } from './employee/employee.module';
-import { DatabaseModule } from './config/database.module'
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-			envFilePath: '../.env',
 		}),
-		DatabaseModule, // глобальный доступ к БД
-		EmployeeModule,
+		PrismaModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
--- api/src/employee/dto/create-employee.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateEmployeeDto {
-	first_name: string;
-	last_name: string;
-	middle_name: string;
-	birth_date: string; // строка в формате DD/MM/YYYY
-}
